c4dbac1f0063782dface16e5e33f2acdf592764f
ukafranklin <111177539+ukafranklin@users.noreply.github.com>
2023-10-02 23:55:59 +0100

    Mail link

--- src/components/Call-to-action/call-to-action.jsx
+++ src/components/Call-to-action/call-to-action.jsx
@@ -26,7 +26,7 @@ const CallToAction = ({img}) => {
 
             <div className="col-md-4 col-lg-3 valign">
               <Link
-                href={`/contact/contact-dark`}
+                href={`mailto:ukafrank@gmail.com`}
               >
                 <a
                   className="butn bord curve wow fadeInUp"
