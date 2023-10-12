8d08c84bc9ffdf04d3ee76ada042603e9c88b425
ukafranklin <111177539+ukafranklin@users.noreply.github.com>
2023-10-12 21:23:26 +0100

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,11 +15,11 @@ class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="keywords"
-            content="HTML5 Template Vie onepage themeforest"
+            content="Design and Website Portfolio"
           />
           <meta
             name="description"
-            content="Vie - Onepage Multi-Purpose HTML5 Template"
+            content="Franklin Uka - Design Portfolio"
           />
           <meta name="author" content="" />
           <link rel="shortcut icon" href="/img/favicon.ico" />
